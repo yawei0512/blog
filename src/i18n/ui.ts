19676180603a670cdfd10a6152b057f7bc1f8e69
sya233 <mnixyw1234@163.com>
2024-01-04 22:45:04 +0800

    添加博客分类功能

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,11 +5,20 @@ export const LANGUAGES = {
 
 export const DEFAULT_LANGUAGE="zhcn";
 
+export const COLLECTIONS = {
+    'Database in Practice': 'database-in-practice',
+    '数据库应用': 'database-in-practice',
+}
+
 export const UI = {
     zhcn: {
-        'header.title': '个人博客'
+        'header.title': '个人博客',
+        'category.title': '博客分类',
+        'category.name': ['数据库应用']
     },
     en: {
-        'header.title': 'My Blog'
+        'header.title': 'My Blog',
+        'category.title': 'Categories',
+        'category.name': ['Database in Practice']
     }
 }
